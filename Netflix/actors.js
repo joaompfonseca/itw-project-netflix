f0df603dbcb20fb2abed650c92cef1508e22eb74
joaompfonseca <joao.fonseca@ua.pt>
2020-12-24 19:23:46 +0000

    listagem de atores bem podre

---
+++ Netflix/actors.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    function ViewModel() {
+        var self = this;
+
+        self.actor = ko.observable()
+
+        $.ajax({
+            type: "GET",
+            url: "http://192.168.160.58/netflix/api/Actors",
+            success: function (data) {
+                console.log(data)
+                self.actor(data.Actors)
+            }
+        })
+    }
+    ko.applyBindings(ViewModel());
+})
