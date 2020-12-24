8658c7ec32a9382de3603ab56d1a41d0c9471c1a
joaompfonseca <joao.fonseca@ua.pt>
2020-12-24 19:23:46 +0000

    dark theme

---
+++ Netflix/actors.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    function ViewModel() {
+        var self = this;
+
+        self.actor = ko.observable()
+
+        $.ajax({
+            url: "http://192.168.160.58/netflix/api/Actors",
+            success: function (data) {
+                console.log(data)
+                self.actor(data.Actors)
+            }
+        })
+    }
+    ko.applyBindings(ViewModel());
+})
