3aee7fddc6aff4ad9d7111bb7b2ea77716ce47d8
joaompfonseca <joao.fonseca@ua.pt>
2020-12-25 17:51:43 +0000

    Pagina atores

--- Netflix/actors.js
+++ Netflix/actors.js
@@ -1,43 +1,93 @@
 ﻿$(document).ready(function () {
     function ViewModel() {
-        //Set Vars
+        //Vars
         var self = this;
         var uri = "http://192.168.160.58/netflix/api/Actors";
 
+        //Arrays
+        pages = [];
+
+        pagesizes = [
+            { number: "10", value: 10 },
+            { number: "25", value: 25 },
+            { number: "50", value: 50 },
+            { number: "100", value: 100 },
+            { number: "250", value: 250 },
+            { number: "500", value: 500 }
+        ];
+
+        //Binds
+        self.Actors = ko.observable();
+        self.TotalActors = ko.observable();
+        self.Pages = ko.observable();
+        self.TotalPages = ko.observable();
+        self.CurrentPage = ko.observable();
+        self.PageSize = ko.observable();
+        self.HasPrevious = ko.observable();
+        self.HasNext = ko.observable();
+        self.FirstPage = ko.observable();
+        self.LastPage = ko.observable();
+
+        self.NextPage = function () {
+            self.CurrentPage(self.CurrentPage() + 1);
+            self.Update();
+        };
+        self.PreviousPage = function () {
+            self.CurrentPage(self.CurrentPage() - 1);
+            self.Update();
+        };
+        self.FirstPage = function () {
+            self.CurrentPage(1);
+            self.Update();
+        }
+        self.LastPage = function () {
+            self.CurrentPage(self.TotalPages());
+            self.Update();
+        }
         //Load Table
         $.ajax({
             url: "http://192.168.160.58/netflix/api/Actors?page=1&pagesize=10",
             success: function (data) {
-                self.actor(data.Actors);
+                console.log(data);
+                self.Actors(data.Actors);
+                self.TotalActors(data.TotalActors);
+                self.TotalPages(data.TotalPages);
+                self.HasPrevious(data.HasPrevious);
+                self.HasNext(data.HasNext);
+
+                for (i = 1; i <= self.TotalPages(); i++) {
+                    pages.push({ number: i.toString(), value: i });
+                }
+                self.Pages(pages);
             }
-        })
+        });
 
-        //Binds
-        pagesizes = [
-            { name: "10", value: 10 },
-            { name: "25", value: 25 },
-            { name: "50", value: 50 },
-            { name: "100", value: 100 },
-            { name: "250", value: 250 },
-            { name: "500", value: 500 }
-        ]
 
-        self.actor = ko.observable()
-        self.pagesize = ko.observable()
-
-        self.update = function () {
-            var pagesize = self.pagesize()['value']
-            console.log(self.pagesize()['value'])
-            uri = uri + '?page=1&pagesize=' + pagesize;
+        //Update Table
+        self.Update = function () {
+            var page = self.CurrentPage();
+            var pagesize = self.PageSize();
+            uri = uri + '?page=' + page + '&pagesize=' + pagesize;
+            pages = [];
             $.ajax({
                 url: uri,
                 success: function (data) {
                     console.log(data);
-                    self.actor(data.Actors);
+                    self.Actors(data.Actors);
+                    self.TotalPages(data.TotalPages);
+                    self.HasPrevious(data.HasPrevious);
+                    self.HasNext(data.HasNext);
+
+                    for (i = 1; i <= self.TotalPages(); i++) {
+                        pages.push({ number: i.toString(), value: i });
+                    }
+                    self.Pages(pages);
                 }
             })
             uri = "http://192.168.160.58/netflix/api/Actors";
         }
+
+        
     }
     ko.applyBindings(ViewModel());
 })
