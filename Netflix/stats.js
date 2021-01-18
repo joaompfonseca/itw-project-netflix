a3eec3bbd7bab9a15f6374eec254a570eb52c6a5
joaompfonseca <joao.fonseca@ua.pt>
2021-01-18 15:57:47 +0000

    stats done!

--- Netflix/stats.js
+++ Netflix/stats.js
@@ -1,10 +1,11 @@
 ﻿$(document).ready(function () {
     console.log("INFO: DOCUMENT READY!"); console.log("");
 
-    //Gráfico
-    google.load("visualization", "1.1", { packages: ["bar"] });
-    google.setOnLoadCallback(drawStuff);
-    function drawStuff() {
+    google.load("visualization", "1.1", { packages: ["bar", "corechart"]});
+    google.setOnLoadCallback(drawStuff1);
+    google.setOnLoadCallback(drawStuff2);
+    //Gráfico 1
+    function drawStuff1() {
         //Pedido AJAX
         var url = 'http://192.168.160.58/netflix/api/Statistics'
 
@@ -17,7 +18,7 @@
                 var countries = data.Countries;
 
                 var data = new google.visualization.arrayToDataTable([
-                    ["Tipo", ''],
+                    ["",""],
                     ["Títulos", titles],
                     ["Atores", actors],
                     ["Diretores", directors],
@@ -31,13 +32,44 @@
                     bar: { groupWidth: "100%" },
                     colors: ['#ffffff'],
                 };
-                var chart = new google.charts.Bar(document.getElementById('chart'));
+                var chart = new google.charts.Bar(document.getElementById('chart1'));
                 chart.draw(data, options);
 
                 console.log("STATS: DONE!");
         }).fail(function () {
             console.log("STATS: FAIL!");
         })
+    };
+
+    //Gráfico 2
+    function drawStuff2() {
+        //Pedido AJAX
+        var url = 'http://192.168.160.58/netflix/api/Statistics/TitlesPerCountry'
+
+        $.get(url)
+            .done(function (data) {
+                var array = data;
+                var newArray = [["",""]];
+                for (i in array) {
+                    newArray.push([array[i].Key, array[i].Value])
+                }
+
+                var data = new google.visualization.arrayToDataTable(newArray);
+                var options = {
+                    legend: { position: 'none' },
+
+                    bars: 'horizontal',
+                    bar: { groupWidth: "100%" },
+                    sliceVisibilityThreshold: 1 / 300,
+                    pieHole: 0.2,
+                };
+                var chart = new google.visualization.PieChart(document.getElementById('chart2'));
+                chart.draw(data, options);
+
+                console.log("STATS: DONE!");
+            }).fail(function () {
+                console.log("STATS: FAIL!");
+            })
     };
 
     //-----PageLoading
